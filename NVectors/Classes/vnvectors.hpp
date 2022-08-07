53318834c6d9c95ba3f9f8b7f383b8a04946c07c
andres <andrumen1@yahoo.com>
2022-08-07 18:29:54 -0500

    Added overloads for the addition operator.

--- NVectors/Classes/vnvectors.hpp
+++ NVectors/Classes/vnvectors.hpp
@@ -49,22 +49,94 @@ namespace VNVectors
 
         /**
          * Addition operator overload. To add a scalar quantity to the current
-         * NVector.
+         * VNVector.
          * 
-         * @param vector The basis vector to be added.
+         * @param vector The basis vector of NVectors to be added.
          * 
          * @param value The value to be added.
          *  
-         * @return A copy of the vector with the value added to each of its 
-         * entries.
+         * @return A copy of the vector of NVectors with the value added to each
+         * of its entries.
         */
-        friend NVector<T> operator + (VNVector<T> vector, T value)
+        friend VNVectors<T> operator + (VNVectors<T> vector, T value)
         {
             // Add the value to the vector.
-            for(size_t i = 0; i < vector.size(); ++i) vector[i] += value;
+            for(size_t i = 0; i < vector.size(); ++i) 
+                vector[i] = vector[i] + value;
 
             return vector;
         }
+
+
+        /**
+         * Addition operator overload. To add an NVector to the current 
+         * NVectors in the VNVector.
+         * 
+         * @param vector The basis vector of NVectors to be added.
+         * 
+         * @param value The NVector to be added.
+         *  
+         * @return A copy of the vector of NVectors with the value added to each
+         * of its entries.
+        */
+        friend VNVectors<T> operator + (
+            VNVectors<T> vector, NVector::NVector<T> value
+        )
+        {
+            // Add the value to the vector.
+            for(size_t i = 0; i < vector.size(); ++i) 
+                vector[i] = vector[i] + value;
+
+            return vector;
+        }
+
+
+        /**
+         * Addition operator overload. To add two VNVectors; they need to have
+         * the same number of NVectors for this to happen.
+         * 
+         * @param vector The vector of NVectors to be added.
+         *  
+         * @return A copy of the vector of NVectors with the value added to each
+         * of its entries.
+        */
+        VNVectors<T> operator + (VNVectors<T> vector)
+        {
+            // Validate the dimensionality of the vector to be added.
+            ValidationGeneral::validateDimensions(
+                vsize, vector.size(), true
+            );
+
+            // Add the value to the vector.
+            for(size_t i = 0; i < vector.size(); ++i) 
+                vector[i] = vector[i] + container[i];
+
+            return vector;
+        }
+
+
+        ////////////////////////////////////////////////////////////////////////
+        // Other Functionality
+        ////////////////////////////////////////////////////////////////////////
+
+
+        /**
+         * Index operator overload. To be able to access the indexes of the
+         * vector of NVectors.
+         * 
+         * @param index The requested index to be accessed.
+        */
+        NVector::NVector<T>& operator [] (size_t index)
+        {   
+            // Auxiliary variables.
+            size_t lower{0}, upper{dimension - 1};
+
+            // Validate the index is in range.
+            ValidationGeneral::validateInRange(index, lower, upper, true);
+
+            return container[index];
+        }
+
 
         //######################################################################
         // Constructor(s) and Destructor(s)
