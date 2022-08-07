abbc4d4abfd8d8b26fe8c64b0351a89ea4a476b7
andres <andrumen1@yahoo.com>
2022-08-06 22:32:26 -0500

    Added more exceptions, GreaterThan.

--- NVectors/Classes/Headers/Exceptions/exceptionsNumerical.hpp
+++ NVectors/Classes/Headers/Exceptions/exceptionsNumerical.hpp
@@ -81,6 +81,68 @@ namespace ExceptionsNumerical
 
 
     /**
+     * Class that builds the exceptions when a given value fails the greater
+     * than comparison.
+    */
+    template <typename T>
+    class GreaterThan : virtual public std::exception
+    {
+        //######################################################################
+        // Public Interface.
+        //######################################################################
+
+
+        public:
+        //----------------------------------------------------------------------
+        // Constructor.
+        //----------------------------------------------------------------------
+
+
+        /**
+         * Constructor for the exception, customizes the exception message.
+         * 
+         * @param value The lower bound to which the variable is being compared.
+         * 
+         * @param variable The variable that is being validated and must be 
+         * greater than the value.
+        */
+        GreaterThan(T value, T variable)
+        {
+            // Create the message.
+            message = "The given variable value, " +
+            std::to_string(variable) + ", is not greater than the required "
+            "value, " + std::to_string(value) + "; i.e., the variable must be " 
+            "greater than the required value.";
+        }
+
+
+        /**
+         * Throws the exception.
+        */
+        virtual const char * what() const throw()
+        {   
+            // Set the custom message.
+            return message.c_str();
+        }
+
+
+        //######################################################################
+        // Private Interface.
+        //######################################################################
+
+
+        private:
+        //----------------------------------------------------------------------
+        // Variables.
+        //----------------------------------------------------------------------
+
+
+        // String that contains the exception message.
+        std::string message{};
+    };
+
+
+    /**
      * Class that builds the exceptions when an index is out of bounds.
     */
     class Numerical : virtual public std::exception
