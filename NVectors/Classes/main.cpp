baecd6e5b875db30b3eb24dcec1081a1767d8e33
andres <andrumen1@yahoo.com>
2022-08-07 21:44:08 -0500

    Nothing of escence.

--- NVectors/Classes/main.cpp
+++ NVectors/Classes/main.cpp
@@ -29,19 +29,16 @@ int main()
 {
     // Auxiliary variables.
     int i = 1;
-    VNVectors::VNVectors<double> vtemp0 = VNVectors::VNVectors<double>(5, 5);
+    VNVectors::VNVectors<double> vtemp0 = VNVectors::VNVectors<double>(3, 5);
 
-    vtemp0[3][2] = 9.9l;
-
-    std::cout << vtemp0 << std::endl;
-    
     // NVectors.
-    NVector::NVector<double> temp0 = NVector::NVector<double>(1);
+    NVector::NVector<double> temp0 = NVector::NVector<double>(3, 1);
     NVector::NVector<double> temp1 = NVector::NVector<double>(3, 7);
     NVector::NVector<double> temp2 = NVector::NVector<double>(3, 8);
      
     temp0 =  (temp1 + temp2) * 0.5;
-
+    std::cout << vtemp0.projection(temp0, true) << std::endl;
+    
     std::cout << "Equal to itself? " << (temp0 == temp0) << std::endl;
     std::cout << "Dot product: " << temp0.dotProduct(temp1) << std::endl;
     std::cout << "Norm squared: " << temp0.normSquared() << std::endl;
@@ -53,7 +50,7 @@ int main()
     temp1[0] = 1.0; temp1[1] = 1.0; temp1[2] = 1.0;
     
     std::cout << "Before normalize: " << temp1 << std::endl;
-    temp0.projection(temp1);
+    std::cout << "Projection: " << temp0.projection(temp1, false) << std::endl;
     std::cout << "After Projection: " << temp1 << std::endl;
 
     // Test the cross product.
